964ed66de5612d5ae78e99f1a609384b1f8f2dea
zahra <h2oedianzahra@gmail.com>
2026-02-25 20:34:27 +0330

    setting

--- shop/settings.py
+++ shop/settings.py
@@ -74,13 +74,11 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 import os
 import dj_database_url
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
+    "default": dj_database_url.config(default= os.environ.get('DATABASE_URL'))
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
